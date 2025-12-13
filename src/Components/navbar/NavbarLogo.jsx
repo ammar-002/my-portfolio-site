05c40be216b193e0e6ab0c7716bca1e60158fd1c
Muhammad Ammar <iammuhammadammar2@gmail.com>
2025-12-14 02:22:37 +0500

    Update NavbarLogo component to wrap name in anchor tag for better accessibility

--- src/Components/navbar/NavbarLogo.jsx
+++ src/Components/navbar/NavbarLogo.jsx
@@ -4,7 +4,7 @@ const NavbarLogo = () => {
   return (
      <div  className='cursor-pointer'>
       <h1 className="text-white text-2xl hidden lg:block font-extrabold">
-       Muhammad Ammar
+       <a href="#">Muhammad Ammar</a>
       </h1>
       <h1 className="text-white font-special font-extrabold text-4xl md:block lg:hidden sm:block">
         M.A
