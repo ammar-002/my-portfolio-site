939b73fb2376a2d12328d3ad5892c78fc3570e91
Muhammad Ammar <iammuhammadammar2@gmail.com>
2026-04-17 18:10:39 +0500

    implemenet chatbot for 24/7 assistant

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import NavbarMain from './Components/navbar/NavbarMain'
 import HeroMain from './Components/HeroSection/HeroMain'
 import SubHero from './Components/Subhero/SubHero'
@@ -8,22 +8,45 @@ import Helper from './Components/HelperSection/Helper'
 import ProjectsMain from './Components/Projects/ProjectsMain'
 import ContectMain from './Components/ContactSection/ContectMain'
 import Footer from './Components/Footer/Footer'
+import '@n8n/chat/style.css'
+import './index.css'
+ 
+import { createChat } from '@n8n/chat'
 
 const App = () => {
+
+  useEffect(() => {
+    createChat({
+      webhookUrl: 'https://muhammadammar.me/webhook/910b6b94-9589-4aa3-ab74-8abdbdc80a9f/chat',
+
+      initialMessages: [
+        'Hi Visitor 👋',
+        'Ask me anything regarding my portfolio!'
+      ],
+
+      i18n: {
+        en: {
+          title: 'Welcome to Portfolio Assistant 👋',
+          footer: '',
+          getStarted: 'New Conversation',
+          inputPlaceholder: 'Type your query..',
+        },
+      },
+    })
+  }, [])
+
   return (
-    <div className=''>
+    <div>
       <NavbarMain />
       <HeroMain />
       <SubHero />
       {/* <AboutMain /> */}
-      <SkillsMain  />
+      <SkillsMain />
       <ProjectsMain />
       <ContectMain />
       <Footer />
       <Helper />
-
     </div>
-    
   )
 }
 
